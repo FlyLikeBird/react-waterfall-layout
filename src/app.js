8f62389668a437e72c0a3d74618925935f435323
FlyLikeBird <renshanhang@163.com>
2020-01-14 23:47:29 +0800

    v1.0

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ButtonComponent from './src/components/button';
+
+export default class App extends React.Component {
+    render(){
+        
+        return (
+            <div>
+            <ButtonComponent />
+            <p>lalala</p>
+            </div>
+        )
+    }
+}
+
+
